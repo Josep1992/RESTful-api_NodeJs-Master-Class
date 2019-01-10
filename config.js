e8504518250567320355b86bb71d35d7b4cf91e2
Josep López <jayden.haylie@gmail.com>
2019-01-09 23:54:04 -0400

    Adding HTTP and HTTPS support

--- config.js
+++ config.js
@@ -2,22 +2,20 @@ const environments = {}
 
 environments.staging = {
   PORT: process.env.PORT || 3000,
+  HTTP: 3000,
+  HTTPS: 3001,
   envNAME: 'staging',
 }
 
 environments.production = {
   PORT: process.env.PORT || 5000,
+  HTTP: 5000,
+  HTTPS: 5001,
   envNAME: 'production',
 }
 
-const currentEnv =
-  typeof process.env.NODE_ENV === 'string'
-    ? process.env.NODE_ENV.toLocaleLowerCase()
-    : ''
+const currentEnv = typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV.toLocaleLowerCase() : ''
 
-const envToExport =
-  typeof environments[currentEnv] === 'object'
-    ? environments[currentEnv]
-    : environments.staging
+const envToExport = typeof environments[currentEnv] === 'object' ? environments[currentEnv] : environments.staging
 
 module.exports = envToExport
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http')
+const https = require('https')
+const fs = require('fs')
 const url = require('url')
-const { PORT, envNAME } = (config = require('./config'))
+const { HTTP, HTTPS } = (config = require('./config'))
 const StringDecoder = require('string_decoder').StringDecoder
 
 const log = (arg) => console.log(arg)
@@ -20,60 +22,72 @@ const router = {
   sample: handlers.sample,
 } // Defining the router
 
+const unifiedServer = function(req, res) {
+  const parsedUrl = url.parse(req.url, true) //Parsing url
+  const path = parsedUrl.pathname //Getting Path
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '') //Removing Slashes from path
+
+  const queryStringObject = parsedUrl.query //Get query string object from url
+  const method = req.method.toLocaleLowerCase() //Http Methods
+  const headers = req.headers //Getting the headers
+
+  const decoder = new StringDecoder('utf-8') // if Payload
+  let buffer = ''
+
+  req.on('data', (data) => (buffer += decoder.write(data))) // Sends received data decoded as utf-8
+
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    const chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound
+
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    } // Data Object to send upon request
+
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode === 'number' ? statusCode : 200 //Use the status code called back by the handler or default to 200
+      payload = typeof payload === 'object' ? payload : {} // Use the payload called back by handler of default to empty object
+
+      const payloadString = JSON.stringify(payload) //Convert payload to string
+
+      // Return response
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+
+      log({
+        'requested at': trimmedPath,
+        'with method': method,
+        'with headers': headers,
+        'with payload': buffer,
+        'and query parameters': queryStringObject,
+        response: {
+          statusCode,
+          payloadString,
+        },
+      })
+    }) // Route the request to the handler specified on the router
+  })
+}
+
 const server = http
-  .createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true) //Parsing url
-    const path = parsedUrl.pathname //Getting Path
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '') //Removing Slashes from path
-
-    const queryStringObject = parsedUrl.query //Get query string object from url
-    const method = req.method.toLocaleLowerCase() //Http Methods
-    const headers = req.headers //Getting the headers
-
-    const decoder = new StringDecoder('utf-8') // if Payload
-    let buffer = ''
-
-    req.on('data', (data) => (buffer += decoder.write(data))) // Sends received data decoded as utf-8
-
-    req.on('end', () => {
-      buffer += decoder.end()
-
-      const chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound
-
-      const data = {
-        trimmedPath,
-        queryStringObject,
-        method,
-        headers,
-        payload: buffer,
-      } // Data Object to send upon request
-
-      chosenHandler(data, (statusCode, payload) => {
-        statusCode = typeof statusCode === 'number' ? statusCode : 200 //Use the status code called back by the handler or default to 200
-        payload = typeof payload === 'object' ? payload : {} // Use the payload called back by handler of default to empty object
-
-        const payloadString = JSON.stringify(payload) //Convert payload to string
-
-        // Return response
-        res.setHeader('Content-Type', 'application/json')
-        res.writeHead(statusCode)
-        res.end(payloadString)
-
-        log({
-          'requested at': trimmedPath,
-          'with method': method,
-          'with headers': headers,
-          'with payload': buffer,
-          'and query parameters': queryStringObject,
-          response: {
-            statusCode,
-            payloadString,
-          },
-        })
-      }) // Route the request to the handler specified on the router
-    })
+  .createServer((req, res) => unifiedServer(req, res))
+  .listen(HTTP, (err) => {
+    if (!err) console.log(`Server started on port: ${HTTP}.`)
   })
 
-  .listen(PORT, (err) => {
-    if (!err) console.log(`Server started on port: ${PORT} in ${envNAME} mode`)
+const httpsOptions = {
+  CERT: fs.readFileSync('./https/cert.pem'),
+  KEY: fs.readFileSync('./https/key.pem'),
+}
+
+const httpsServer = https
+  .createServer(httpsOptions, (req, res) => unifiedServer(req, res))
+  .listen(HTTPS, (err) => {
+    if (!err) console.log(`Server started on port: ${HTTPS}.`)
   })
